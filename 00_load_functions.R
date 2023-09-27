cf5056451232bca3b50267250a8e8f3d9cf2daa1
mtopolska <88841879+mtopolska@users.noreply.github.com>
2023-09-27 19:41:46 +0200

    Update 00_load_functions.R

--- 00_load_functions.R
+++ 00_load_functions.R
@@ -1,8 +1,9 @@
 
 ## 1)
-## download the all_functions folder containing all the scripts with functions needed to run the analysis
+## download the deep_indel_mutagenesis/Functions folder containing all the scripts with functions needed to run the analysis
 
-folder_location <- "~/Desktop/mount1/indel2fold/many_domains/joined_analysis/doms_joined_analysis_fold_longer_selection/analysis/code_manuscript/functions/all_functions"
+# set folder location; replace ".." with your folder path
+folder_location <- "~/../Functions"
 script_files <- list.files(path = folder_location, pattern = "\\.R$", full.names = TRUE)
 
 for (script_file in script_files) {
@@ -11,7 +12,9 @@ for (script_file in script_files) {
 
 ## 2)
 ## load data necessary to run all the functions in the next scripts: additional_dfs.rds
-tmp=readRDS(file="~/Desktop/mount1/indel2fold/many_domains/joined_analysis/doms_joined_analysis_fold_longer_selection/analysis/code_manuscript/functions/additional_dfs.rds")
+
+# load the additional_dfs.rds; replace ".." with you folder path
+tmp=readRDS(file="~/../additional_dfs.rds")
 
 CADD_subs = tmp[[1]]
 CADD_insA = tmp[[2]]
@@ -33,22 +36,25 @@ validated_domains_in_vitro_ddGs_and_growthrates = tmp[[17]]
 cor_tsuboyamaVStopolska = tmp[[18]]
 
 ## 3)
-### download the analysis folder and set the different paths. 
-## folder where the dimsum files are
-dimsum_file_path <- "~/Desktop/mount1/indel2fold/many_domains/joined_analysis/doms_joined_analysis_fold_longer_selection/analysis/code_manuscript/functions/DimSum/"
+## download the DiMSum raw data folder set the path; replace ".." with your folder path
+dimsum_file_path <- "~/../DiMSum/"
 
-## folder where you want to save the final data frames
-output_location_aPCA<-"~/Desktop/mount1/indel2fold/many_domains/joined_analysis/doms_joined_analysis_fold_longer_selection/analysis/code_manuscript/functions/"
-output_location_bPCA<-"~/Desktop/mount1/indel2fold/many_domains/joined_analysis/doms_joined_analysis_fold_longer_selection/analysis/code_manuscript/functions/"
-output_location_colorscale<-"~/Desktop/mount1/indel2fold/many_domains/joined_analysis/doms_joined_analysis_fold_longer_selection/analysis/code_manuscript/functions/"
-output_location_tsuboyama_processed<-"~/Desktop/mount1/indel2fold/many_domains/joined_analysis/doms_joined_analysis_fold_longer_selection/analysis/code_manuscript/functions/"
+## if you run PART 1-3 (001_split_data): set folder paths to where you want to save the final processed dfs
+## if you run PART4 (001_split_data): set folder paths to where you have downloaded the .rds files
+# replace ".." with your folder path
 
-## folder where your downloaded Tsuboyama data + pdb files are
-data_folder_tsuboyama<-"~/Desktop/indel_pub/External_data/Rocklin/Processed_K50_dG_datasets/"
-data_folder_tsyboyama_PDBs<-"~/Desktop/mount1/Rocklin/Rocklin_pdbs/"
+output_location_aPCA <-"~/../"
+output_location_bPCA <-"~/../"
+output_location_colorscale <-"~/../"
+output_location_tsuboyama_processed <-"~/../"
 
-## where to save the data encoded to run the predictor
-output_location_ddGencoded<-"~/Desktop/mount1/indel2fold/many_domains/joined_analysis/doms_joined_analysis_fold_longer_selection/analysis/code_manuscript/functions/"
+## folder path where your downloaded Tsuboyama raw data + folder with pdb_files
+# replace ".." with your folder path
+data_folder_tsuboyama <- "~/../Rocklin/Processed_K50_dG_datasets/"
+data_folder_tsyboyama_PDBs <- "~/../AlphaFold_model_PDBs"
+
+## folder path to save the df for running the predictor/folder path where the downloaded .rds is
+output_location_ddGencoded<-"~/../"
 
 
 
