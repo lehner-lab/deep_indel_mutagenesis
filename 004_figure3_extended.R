cd93fa287d58baa851f849a6cbe8f3a3616898d6
mtopolska <88841879+mtopolska@users.noreply.github.com>
2023-09-28 18:52:07 +0200

    Update 004_figure3_extended.R

--- 004_figure3_extended.R
+++ 004_figure3_extended.R
@@ -9,7 +9,7 @@ library(gridExtra)
 #########################################################################################################
 ##### hist plots a), correlation to rSASA but divided into loop and structure. 
 
-#subs
+## substitutions
 struc<-tsuboyama_nat_doms_all[tsuboyama_nat_doms_all$simple_struc %in% c("310Helix", "AlphaHelix", "Strand"),]
 loop<-tsuboyama_nat_doms_all[!tsuboyama_nat_doms_all$simple_struc %in% c("310Helix", "AlphaHelix", "Strand"),]
 
@@ -70,7 +70,7 @@ legend("left",
        bty = "n"
 )
 
-## dels
+## deletions
 tsyboyama_rsasa_del_struc<-c()
 for (i in unique(struc$pdb_name)){
   if (nrow(struc[struc$pdb_name==i,])>3){
@@ -128,7 +128,7 @@ legend("left",
        bty = "n"
 )
 
-## ins
+## insertions
 tsyboyama_rsasa_ins_struc<-c()
 for (i in unique(struc$pdb_name)){
   if (nrow(struc[struc$pdb_name==i,])>3){
@@ -190,19 +190,24 @@ legend("left",
 
 #########################################################################################################
 #########################################################################################################
-##### b) effects of indels/subs on ntermini: divide into short and long termini
+##### effects of indels/subs on ntermini: divide into short and long termini b)
+
+## isolate n-termini data points
 ntermini_ddG<-tsuboyama_nat_doms_all[tsuboyama_nat_doms_all$simple_struc=="ntermini",]
-## boxplot:
+
+## boxplot for short termini 1-3 aa:
+# count unique terminis
 unique_counts <- aggregate(element_no_simple ~ align_to_center, data = ntermini_ddG[ntermini_ddG$element_lenght%in% c(1:3),], FUN = function(x) length(unique(x)))
 
 result <- termini_plot(ntermini_ddG[ntermini_ddG$element_lenght%in% c(1:3),],
               ntermini_ddG[ntermini_ddG$element_lenght%in% c(1:3),]$ddG_ML_subs,
               unique_counts)
 
+# save plot as short
 short <- result[[1]]
 
-
-##### collapse the last residues
+## boxplot for long termini 
+# collapse the residues farthest away from the secondary structure (no effect)
 # Collapse data for align_to_center from -7 to -14
 ntermini_ddG$align_to_center <- ifelse(
   ntermini_ddG$align_to_center %in% c(-6:-18),
@@ -214,20 +219,22 @@ ntermini_ddG$align_to_center <- factor(
   ntermini_ddG$align_to_center,
   levels = c("-18:-6","-5","-4","-3","-2","-1")
 )
-
+                           
+# count unique terminis
 unique_counts <- aggregate(element_no_simple ~ align_to_center, data = ntermini_ddG[ntermini_ddG$element_lenght %in% 4:14, ], FUN = function(x) length(unique(x)))
 
 result <- termini_plot(ntermini_ddG[ntermini_ddG$element_lenght%in% c(4:14),],
                         ntermini_ddG[ntermini_ddG$element_lenght%in% c(4:14),]$ddG_ML_subs,
                         unique_counts)
+# save plot as long
 long <- result[[1]]
 long <- long+ylab("ddG substitutions")+theme(axis.text.y = element_text(size=16),
                                              axis.title.y = element_text(size=18,angle=90))
 
-## combine
+## combine both short and long and plot
 combined_plot <- grid.arrange(long, short, ncol = 2, widths = c(3, 2))
 
-## insertions
+## same for insertions:
 unique_counts <- aggregate(element_no_simple ~ align_to_center, data = ntermini_ddG[ntermini_ddG$element_lenght%in% c(1:3),], FUN = function(x) length(unique(x)))
 result <- termini_plot(ntermini_ddG[ntermini_ddG$element_lenght%in% c(1:3),],
                         ntermini_ddG[ntermini_ddG$element_lenght%in% c(1:3),]$ddG_ML_ins,
@@ -244,10 +251,10 @@ long <- result[[1]]
 long <- long+ylab("ddG insertions")+theme(axis.text.y = element_text(size=16),
                                              axis.title.y = element_text(size=18,angle=90))
 
-## combine
+## combine both short and long and plot
 combined_plot <- grid.arrange(long, short, ncol = 2, widths = c(3, 2))
 
-## deletions
+## same for deletions:
 unique_counts <- aggregate(element_no_simple ~ align_to_center, data = ntermini_ddG[ntermini_ddG$element_lenght%in% c(1:3),], FUN = function(x) length(unique(x)))
 result <- termini_plot(ntermini_ddG[ntermini_ddG$element_lenght%in% c(1:3),],
                         ntermini_ddG[ntermini_ddG$element_lenght%in% c(1:3),]$ddG_ML_dels,
@@ -264,12 +271,13 @@ long <- result[[1]]
 long <- long+ylab("ddG deletions")+theme(axis.text.y = element_text(size=16),
                                           axis.title.y = element_text(size=18,angle=90))
 
-## combine
+## combine both short and long and plot
 combined_plot <- grid.arrange(long, short, ncol = 2, widths = c(3, 2))
 
 #########################################################################################################
 #########################################################################################################
-##### b) effects of indels/subs on ctermini: divide into short and long termini
+##### effects of indels/subs on ctermini: divide into short and long termini b)
+
 ctermini_ddG<-tsuboyama_nat_doms_all[tsuboyama_nat_doms_all$simple_struc=="ctermini",]
 ## boxplot:
 unique_counts <- aggregate(element_no_simple ~ align_to_center, data = ctermini_ddG[ctermini_ddG$element_lenght%in% c(1:3),], FUN = function(x) length(unique(x)))
@@ -280,8 +288,8 @@ result <- termini_plot(ctermini_ddG[ctermini_ddG$element_lenght%in% c(1:3),],
 
 short <- result[[1]]
 
-
-##### collapse the last residues
+## boxplot for long termini 
+# collapse the residues farthest away from the secondary structure (no effect)
 # Collapse data for align_to_center from -7 to -14
 ctermini_ddG$align_to_center <- ifelse(
   ctermini_ddG$align_to_center %in% c(6:8),
@@ -302,10 +310,10 @@ result <- termini_plot(ctermini_ddG[ctermini_ddG$element_lenght%in% c(4:14),],
 long <- result[[1]]
 long <- long+theme(axis.text.y = element_text(size=16))
 
-## combine
+## combine both short and long and plot
 combined_plot <- grid.arrange(long, short, ncol = 2, widths = c(3, 2))
 
-## insertions
+## same for insertions:
 unique_counts <- aggregate(element_no_simple ~ align_to_center, data = ctermini_ddG[ctermini_ddG$element_lenght%in% c(1:3),], FUN = function(x) length(unique(x)))
 result <- termini_plot(ctermini_ddG[ctermini_ddG$element_lenght%in% c(1:3),],
                         ctermini_ddG[ctermini_ddG$element_lenght%in% c(1:3),]$ddG_ML_ins,
@@ -321,10 +329,10 @@ result <- termini_plot(ctermini_ddG[ctermini_ddG$element_lenght%in% c(4:14),],
 long <- result[[1]]
 long <- long+theme(axis.text.y = element_text(size=16))
 
-## combine
+## combine both short and long and plot
 combined_plot <- grid.arrange(long, short, ncol = 2, widths = c(3, 2))
 
-## deletions
+## same for: deletions
 unique_counts <- aggregate(element_no_simple ~ align_to_center, data = ctermini_ddG[ctermini_ddG$element_lenght%in% c(1:3),], FUN = function(x) length(unique(x)))
 result <- termini_plot(ctermini_ddG[ctermini_ddG$element_lenght%in% c(1:3),],
                         ctermini_ddG[ctermini_ddG$element_lenght%in% c(1:3),]$ddG_ML_dels,
@@ -340,14 +348,14 @@ result <- termini_plot(ctermini_ddG[ctermini_ddG$element_lenght%in% c(4:14),],
 long <- result[[1]]
 long <- long+theme(axis.text.y = element_text(size=16))
 
-## combine
+## combine both short and long and plot
 combined_plot <- grid.arrange(long, short, ncol = 2, widths = c(3, 2))
 
 #########################################################################################################
 #########################################################################################################
-##### c) indel/sub effect on lenght, divided into different elements. 
+##### indel/sub effect on lenght of structure element, divided into different elements. c)
 
-## helix
+## helix: isolate and count helices
 helix_df<-tsuboyama_nat_doms_all[tsuboyama_nat_doms_all$simple_struc %in% c("310Helix", "AlphaHelix"),]
 unique_counts_helix <- aggregate(element_no_simple ~ element_lenght, data = helix_df, FUN = function(x) length(unique(x)))
 
@@ -355,7 +363,7 @@ plot_lenght_fig3(helix_df,
                  unique_counts_helix)+
   xlab("helix lenght")
 
-## strand
+## strand: isolate and count strands
 strand_df<-tsuboyama_nat_doms_all[tsuboyama_nat_doms_all$simple_struc=="Strand",]
 unique_counts_strand <- aggregate(element_no_simple ~ element_lenght, data = strand_df, FUN = function(x) length(unique(x)))
 
@@ -363,7 +371,7 @@ plot_lenght_fig3(strand_df,
                  unique_counts_strand)+
   xlab("strand lenght")
 
-## loop
+## loop: isolate and count loops
 loop_df<-tsuboyama_nat_doms_all[tsuboyama_nat_doms_all$simple_struc=="loop",]
 unique_counts_loop <- aggregate(element_no_simple ~ element_lenght, data = loop_df, FUN = function(x) length(unique(x)))
 
@@ -375,14 +383,14 @@ plot_lenght_fig3(loop_df,
 
 #########################################################################################################
 #########################################################################################################
-##### d) indel/sub effect on structure before or after, divided into structure elements and loop. 
+##### indel/sub effect on structure before or after, divided into structure elements and loop. d)
+                                
 ## divide into structure and loop
 structure_df<-tsuboyama_nat_doms_all[tsuboyama_nat_doms_all$simple_struc %in% c("AlphaHelix", "Strand", "310Helix"),]
 
-# structure before: structured elements. 
+# structure before: secondary structure elements. 
 unique_counts <- aggregate(element_no_simple ~ structure_before, data = structure_df, FUN = function(x) length(unique(x)))
 custom_order <- c("start", "ntermini", "loop", "310Helix", "AlphaHelix", "Strand")
-
 
 plot_neighbour_fig3(structure_df,
                     structure_df$structure_before,
@@ -390,10 +398,9 @@ plot_neighbour_fig3(structure_df,
                     unique_counts$structure_before)+
   xlab("structure element before")
 
-# structure after: structured elements. 
+# structure after: secondary structure elements. 
 unique_counts <- aggregate(element_no_simple ~ structure_after, data = structure_df, FUN = function(x) length(unique(x)))
 custom_order <- c("ctermini", "loop", "310Helix", "AlphaHelix", "Strand")
-
 
 plot_neighbour_fig3(structure_df,
                     structure_df$structure_after,
@@ -401,13 +408,11 @@ plot_neighbour_fig3(structure_df,
                     unique_counts$structure_after)+
   xlab("structure element after")
 
-## loop
+# structure before: loop.
 loop_df<-tsuboyama_nat_doms_all[!tsuboyama_nat_doms_all$simple_struc %in% c("AlphaHelix", "Strand", "310Helix"),]
 
-# structure before: structured elements. 
 unique_counts <- aggregate(element_no_simple ~ structure_before, data = loop_df, FUN = function(x) length(unique(x)))
 custom_order <- c("start", "310Helix", "AlphaHelix", "Strand")
-
 
 plot_neighbour_fig3(loop_df,
                     loop_df$structure_before,
@@ -415,10 +420,9 @@ plot_neighbour_fig3(loop_df,
                     unique_counts$structure_before)+
   xlab("structure element before")
 
-# structure after: structured elements. 
+# structure after: loop.
 unique_counts <- aggregate(element_no_simple ~ structure_after, data = loop_df, FUN = function(x) length(unique(x)))
 custom_order <- c("end", "310Helix", "AlphaHelix", "Strand")
-
 
 plot_neighbour_fig3(loop_df,
                     loop_df$structure_after,
