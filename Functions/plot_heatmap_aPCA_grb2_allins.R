9d90bdc1a66d182bd1f1cb6bbba9c363ed48016b
mtopolska <88841879+mtopolska@users.noreply.github.com>
2023-10-02 15:33:43 +0200

    Update plot_heatmap_aPCA_grb2_allins.R

--- Functions/plot_heatmap_aPCA_grb2_allins.R
+++ Functions/plot_heatmap_aPCA_grb2_allins.R
@@ -9,27 +9,25 @@ plot_heatmap_aPCA_grb2_allins <- function(){
                   fill=scaled_fitness))+
     scale_fill_gradient2(low="red",mid="white",high="blue",midpoint=1,
                          limits=c(min(na.omit(color_scale)),max(na.omit(color_scale))))+
-    theme(axis.text.x = element_text(size = 11, 
-                                     vjust = 0.5, 
-                                     hjust = 0.5, 
-                                     angle = 0), 
-          panel.border = element_blank(),
-          panel.grid.major = element_blank(),
-          panel.grid.minor = element_blank())+
     scale_x_discrete(limits=c("1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31","32","33","34","35","36","37","38","39","40","41","42","43","44","45","46","47","48","49","50","51","52"))+
     ylab("grb2_fold")+
     geom_text(data=grb2_fold_df[grb2_fold_df$type == "allins",], 
               aes(x=Pos,
                   y=insID,
                   label=grb2_fold_df[grb2_fold_df$type == "allins",]$significant))+
-    theme(axis.title.y = element_text(size=18),
-          axis.text.y = element_text(size=16))+
     theme_gray()+
     theme(panel.grid.major = element_blank(), 
           panel.grid.minor = element_blank(), 
           axis.title.x = element_blank(),
           axis.title.y = element_blank(),
-          legend.position = "none")
+          legend.title = element_blank(),
+          legend.position = "none",
+          axis.text.y = element_text(size=16),
+          axis.text.x = element_text(size = 11, 
+                                     vjust = 0.5, 
+                                     hjust = 0.5, 
+                                     angle = 0), 
+          panel.border = element_blank())
   
   return(list(grb2_fold_allins_hm = grb2_fold_allins_hm))
   
